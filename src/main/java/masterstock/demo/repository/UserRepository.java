085c0747420ceee54253bc0513dc7573b8123810
User <user@MacBook-Pro-de-User.local>
2025-09-13 15:07:04 +0100

    AuthorizationService

--- src/main/java/masterstock/demo/repository/UserRepository.java
+++ src/main/java/masterstock/demo/repository/UserRepository.java
@@ -2,9 +2,11 @@ package masterstock.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
 import masterstock.demo.entity.user.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,String>{
     UserDetails findByLogin (String login);
 }
