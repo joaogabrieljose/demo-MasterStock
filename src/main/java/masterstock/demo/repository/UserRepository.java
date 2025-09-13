18934c0da2b2eedbf7b46d8e6e974edf812a6c23
User <user@MacBook-Pro-de-User.local>
2025-09-13 15:01:35 +0100

    userRepository

---
+++ src/main/java/masterstock/demo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package masterstock.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import masterstock.demo.entity.user.User;
+
+public interface UserRepository extends JpaRepository<User,String>{
+    UserDetails findByLogin (String login);
+}
